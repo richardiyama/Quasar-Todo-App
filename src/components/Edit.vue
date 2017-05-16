f9f1388377e353ebc2827fe854f30ee237a8da13
richard iyama <richard.iyama@orbsolutions.com.ng>
2017-05-16 16:47:54 +0100

    commit Edit interface

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -4,8 +4,7 @@
          class="toolbar">
       <button class="default"
               @click="clickMethod()">
-        <i id="tag"
-           class="material-icons">keyboard_arrow_left</i>
+        <i id="tag">keyboard_arrow_left</i>
       </button>
   
       <label id="Back">Home</label>
@@ -20,165 +19,146 @@
     </div>
   
     <div class="layout-view">
-      
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-            
-                <i id="tagline7"
-                   class="material-icons">radio_button_unchecked</i><span id="tagline6">Refinish basement</span>
-                <i id="tagline8"
-                   class="material-icons">star_border</i>
-              </div>
-            </div>
-          </div>
-        
-      
   
-      
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-              
-                <i id="tagline10"
-                   class="material-icons">description</i><span id="tagline11">5/8" on ceiling 1/4" on wall</span>
-                <i id="tagline12"
-                   class="material-icons">chevron_right</i>
-              </div>
-            </div>
-          </div>
-        
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
   
-      
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-            
-                <i id="tagline10"
-                   class="material-icons">input</i>Action<i id="tagline13"
-                   class="material-icons">person</i>
-                <i id="tagline15"
-                   class="material-icons">local_phone</i><i id="tagline16"
-                   class="material-icons">room</i> <i id="tagline16"
-                   class="material-icons">assignment</i>
-              </div>
-            </div>
+            <i style="width: 100px;font-size: 30px; color: #2095f4">radio_button_unchecked</i>
+            <div class="auto"
+                 style="padding-left: 10px;font-size: 18px">Refinish basement</div>
+            <i style="font-size: 40px; color: darkgray;">star_border</i>
           </div>
-      
-
-      
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-                <i id="tagline10"
-                   class="material-icons">account_circle</i><span id="tagline18">Me</span>
-                <i id="tagline19" class="material-icons">close</i>
-              </div>
-            </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+  
+            <i style="font-size: 20px; color: #2095f4">description</i>
+            <div class="auto"
+                 style="padding-left: 10px">5/8" on ceiling 1/4" on wall</div>
+            <i style="font-size: 35px; color: darkgray">chevron_right</i>
           </div>
-        
-      
-
-
-      
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-              <i id="tagline20"
-                   class="fa fa-comments"></i>
-                <span id="tagline6">Add a comment</span><i id="tagline21"
-                   class="material-icons">chevron_right</i>
-              </div>
-            </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+  
+            <i style="font-size: 20px; color: darkgray">input</i>
+            <div class="auto"
+                 style="padding-left: 15px;  color: darkgray">Action</div>
+            <div class="auto"
+                 style="padding-left: 25px;font-size: 25px;  color: darkgray"><i>person</i></div>
+            <div class="auto"
+                 style="padding-left: 20px; font-size: 25px; transform: rotate(275deg);color: darkgray"><i>local_phone</i></div>
+            <div class="auto"
+                 style="padding-left: 40px; font-size: 25px; color: darkgray "><i>room</i></div>
+            <div class="auto"
+                 style="padding-left: 50px; font-size: 30px; color: darkgray"> <i>link</i></div>
           </div>
-  <br>
-  <br>
-
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-                <i id="tagline10"
-                   class="material-icons">input</i>Action<i id="tagline13"
-                   class="material-icons">person</i>
-                <i id="tagline15"
-                   class="material-icons">local_phone</i><i id="tagline16"
-                   class="material-icons">room</i> <i id="tagline16"
-                   class="material-icons">assignment</i>
-              </div>
-            </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+            <i style="font-size: 20px; color: #2095f4">account_circle</i>
+            <div class="auto"
+                 style="padding-left: 10px; color: #2095f4">Me</div>
+            <i style="font-size: 30px; color: #2095f4">close</i>
           </div>
-        
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-                <i id="tagline10"
-                   class="material-icons">input</i>Action<i id="tagline13"
-                   class="material-icons">person</i>
-                <i id="tagline15"
-                   class="material-icons">local_phone</i><i id="tagline16"
-                   class="material-icons">room</i> <i id="tagline16"
-                   class="material-icons">assignment</i>
-              </div>
-            </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+            <i class="ico fa fa-comments" style="font-size: 20px; color: darkgray"></i>
+            <div class="auto"
+                 style="padding-left: 10px; color: darkgray">Add a comment</div>
+            <i style="font-size: 35px; color: darkgray">chevron_right</i>
           </div>
-
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-                <i id="tagline10"
-                   class="material-icons">input</i>Action<i id="tagline13"
-                   class="material-icons">person</i>
-                <i id="tagline15"
-                   class="material-icons">local_phone</i><i id="tagline16"
-                   class="material-icons">room</i> <i id="tagline16"
-                   class="material-icons">assignment</i>
-              </div>
-            </div>
+        </div>
+      </div>
+      <br>
+      <br>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+            <i style="font-size: 25px; color: #2095f4">event_available</i>
+            <div class="auto"
+                 style="padding-left: 10px; color: #2095f4">Due</div>
+            <div class="auto"
+                 style="padding-left: 80px; color: #2095f4">Wed Jun 8</div>
+            <i style="font-size: 30px; color: #2095f4">close</i>
           </div>
-
-      
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-                <i id="tagline10"
-                   class="material-icons">input</i>Action<i id="tagline13"
-                   class="material-icons">person</i>
-                <i id="tagline15"
-                   class="material-icons">local_phone</i><i id="tagline16"
-                   class="material-icons">room</i> <i id="tagline16"
-                   class="material-icons">assignment</i>
-              </div>
-            </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+            <i style="font-size: 25px; color: darkgreen">slideshow</i>
+            <div class="auto"
+                 style="padding-left: 10px; color: darkgreen">Start</div>
+            <div class="auto"
+                 style="padding-left: 80px; color: darkgreen">Tomorrow</div>
+            <i style="font-size: 30px; color: darkgreen">close</i>
           </div>
-        
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-                <i id="tagline10"
-                   class="material-icons">input</i>Action<i id="tagline13"
-                   class="material-icons">person</i>
-                <i id="tagline15"
-                   class="material-icons">local_phone</i><i id="tagline16"
-                   class="material-icons">room</i> <i id="tagline16"
-                   class="material-icons">assignment</i>
-              </div>
-            </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+            <i style="font-size: 25px; color: darkgray">notifications</i>
+            <div class="auto"
+                 style="padding-left: 10px; color: darkgray">Add reminder...</div>
           </div>
-
-      
-        <div class="list">
-          <div class="item">
-            <div class="item-content">
-                <i id="tagline10"
-                   class="material-icons">input</i>Action<i id="tagline13"
-                   class="material-icons">person</i>
-                <i id="tagline15"
-                   class="material-icons">local_phone</i><i id="tagline16"
-                   class="material-icons">room</i> <i id="tagline16"
-                   class="material-icons">assignment</i>
-              </div>
-            </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+            <i style="font-size: 25px; color: darkgray">near_me</i>
+            <div class="auto"
+                 style="padding-left: 10px; color: darkgray">Remind me at a location</div>
+            <i style="font-size: 35px; color: darkgray">chevron_right</i>
           </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+            <i class="fa fa-repeat" style="font-size: 25px; color: darkgray"></i>
+            <div class="auto"
+                 style="padding-left: 10px; color: darkgray">Repeat</div>
+          </div>
+        </div>
+      </div>
+  
+      <div class="list">
+        <div class="item">
+          <div class="item-content row">
+            <i style="font-size: 25px; color: darkgray">warning</i>
+            <div class="auto"
+                 style="padding-left: 10px; color: darkgray">Priority</div>
+              <div class="auto"
+                 style="padding-left: 3px"><i style="font-size: 25px; color: darkgray">remove</i>
+              <i style="font-size: 25px; color: darkgray">drag_handle</i>  
+             <i style="font-size: 25px; color: darkgray">menu</i> </div>   
+            <i style="font-size: 30px; color: darkgray">close</i>
+          </div>
+        </div>
+      </div>
+  
     </div>
   
   </q-layout>
@@ -193,6 +173,10 @@ export default {
 </script>
 
 <style>
+.ico {
+  margin-right: 15px;
+}
+
 #LeftBack {
   font-size: 50px;
   width: 50px;
@@ -256,105 +240,5 @@ button.q-search-icon span {
   background: #2196f3;
   font-family: "Roboto", "Helvetica", "Arial", sans-serif;
   font-weight: 500px;
-}
-
-#tagline7 {
-  color: #2095f4;
-  width: 27px;
-  font-size: 30px;
-  left: 100px;
-  margin: 2px;
-}
-
-#tagline10 {
-  color: #2095f4;
-  width: 50px;
-  font-size: 20px;
-  left: 100px;
-  margin: 2px;
-}
-
-#tagline20 {
-  
-  width: 70px;
-  font-size: 20px;
-  left: 100px;
-  margin: 2px;
-  
-}
-
-#tagline21 {
-
- color: #2095f4;
-  width: 30px;
-  font-size: 30px;
-  left: 100px;
-  margin: 15px;
-  font-weight: lighter;
-  color: lightgray
-  
-}
-
-#tagline11 {
-  margin: 20px;
-  font-size: 15px;
-  line-height: 0;
-}
-
-#tagline16 {
-  margin: 20px;
-  font-size: 15px;
-  line-height: 0;
-}
-
-#tagline13 {
-  margin: 30px;
-  font-size: 15px;
-  line-height: 0;
-}
-
-#tagline12 {
-  color: #2095f4;
-  width: 30px;
-  font-size: 30px;
-  left: 100px;
-  margin: 15px;
-  font-weight: lighter;
-  color: lightgray;
-}
-
-#tagline15 {
-  transform: rotate(270deg);
-}
-
-#tagline6 {
-  margin: 20px;
-  font-size: 20px;
-  line-height: 0;
-}
-#tagline18 {
-  margin: 3px;
-  font-size: 14px;
-  line-height: 0;
-  color: #2095f4;
-  
-}
-
-#tagline19 {
-  margin: 20px;
-  font-size: 30px;
-  line-height: 0;
-  color: #2095f4;
-  
-}
-
-#tagline8 {
-  color: #2095f4;
-  width: 75px;
-  font-size: 40px;
-  left: 100px;
-  margin: 11px;
-  font-weight: lighter;
-  color: lightgray;
 }
 </style>
